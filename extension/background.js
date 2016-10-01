ec7f136b36a97b02bb05278749ace41bcf96df8a
Wayan Jimmy <jimmyeatcrab@gmail.com>
2016-10-02 07:54:32 +0800

    Add blacklist

--- extension/background.js
+++ extension/background.js
@@ -1,9 +1,21 @@
+'use strict';
+
+const blacklist = [
+	/(share|intent|oauth)/,
+	/^i\/redirect\?url=/
+];
+
 chrome.webRequest.onBeforeRequest.addListener(details => {
 	if (details.method !== 'GET') {
 		return;
 	}
 
 	const url = new URL(details.url);
+	const pathAndQuery = url.pathname.slice(1) + url.search;
+
+	if (blacklist.some(x => x.test(pathAndQuery))) {
+		return;
+	}
 
 	url.hostname = 'm.facebook.com';
 
